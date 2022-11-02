e211405ab955368b8775a499f4ea7f0babb04cee
Khushal Roop Rai <86585376+rai-khushal29@users.noreply.github.com>
2022-11-02 21:41:27 +0530

    Updated for all platforms
    Build for Android and iOS and Windows

--- weather/lib/main.dart
+++ weather/lib/main.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:fluent_ui/fluent_ui.dart' as fluent;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:weather/ios_ui.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather/windows_ui.dart';
 import 'Forecast_Model.dart';
 
 void main() {
@@ -16,10 +19,214 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Weather',
-      home: const MyHomePage(title: 'Weather'),
+    if (Platform.isAndroid || Platform.isIOS || Platform.isMacOS) {
+      return CupertinoApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Weather',
+        home: const MyHomePage(title: 'Weather'),
+      );
+    } else {
+      return fluent.FluentApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Weather',
+        home: const MyWindowsHomePage(title: 'Weather'),
+      );
+    }
+  }
+}
+
+class MyWindowsHomePage extends StatefulWidget {
+  const MyWindowsHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyWindowsHomePage> createState() => _MyWindowsHomePageState();
+}
+
+class _MyWindowsHomePageState extends State<MyWindowsHomePage> {
+  int _pane_one = 0;
+  String forecast_ico = "";
+  String time = "";
+  String city_name = "";
+  String temp = "";
+  String ico_url = "";
+  String desc = "";
+  String fl = "";
+  List<Forecast_Model> _forecast_list = [];
+  String temp_max = 'temp+max';
+  String temp_min = 'temp_min';
+  String pressure = 'pressure';
+  String humidity = 'humidity';
+  String q = "Jodhpur"; // city input
+  late String temp_city; // If city is not found then restore the value
+  TextEditingController tc = TextEditingController();
+
+  callapi() async {
+    try {
+      var url = Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&appid=bebd9da120762f2b44fb9b1f056c90ef");
+      var url1 = Uri.parse(
+          "https://api.openweathermap.org/data/2.5/forecast?q=${q}&units=metric&appid=bebd9da120762f2b44fb9b1f056c90ef");
+      var responseForecast = await http.get(url1);
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        temp_city = q;
+        var data = await jsonDecode(response.body);
+        var forecastData = await jsonDecode(responseForecast.body);
+        print("Response Status: ${response.statusCode}");
+        print("Response body: ${response.body}");
+        city_name = data["name"]!.toString();
+        temp = data["main"]["temp"]!.toString() +
+            String.fromCharCode(0x00B0) +
+            "C";
+        ico_url = "https://openweathermap.org/img/wn/" +
+            data["weather"][0]["icon"]!.toString() +
+            "@2x.png";
+        desc = data["weather"][0]["description"]!.toString();
+        fl = data["main"]["feels_like"]!.toString() +
+            String.fromCharCode(0x00B0) +
+            "C";
+        temp_max = data["main"]["temp_max"]!.toString() +
+            String.fromCharCode(0x00B0) +
+            "C";
+        temp_min = data["main"]["temp_min"]!.toString() +
+            String.fromCharCode(0x00B0) +
+            "C";
+        pressure = data["main"]["pressure"]!.toString();
+        humidity = data["main"]["humidity"]!.toString();
+        time = forecastData["list"][0]["dt_txt"]!.toString();
+        for (int i = 0; i < forecastData["list"].length - 1; i++) {
+          _forecast_list.add(Forecast_Model(
+              forecastData["list"][i]["dt_txt"]!.toString(),
+              "https://openweathermap.org/img/wn/" +
+                  forecastData["list"][i]["weather"][0]["icon"]!.toString() +
+                  "@2x.png",
+              forecastData["list"][i]["weather"][0]["description"]!
+                  .toString()));
+        }
+        windows_ui(
+            key: null,
+            city_name: city_name,
+            temp: temp,
+            fl: fl,
+            temp_max: temp_max,
+            temp_min: temp_min,
+            ico_url: ico_url,
+            desc: desc,
+            pressure: pressure,
+            humidity: humidity,
+            forecast_list: _forecast_list);
+        print(city_name);
+        print(time);
+      } else if (response.statusCode == 404) {
+        q = temp_city;
+        fluent.showDialog(
+            context: context,
+            builder: (context) {
+              return fluent.ContentDialog(
+                title: Text("Weather"),
+                content: Text("Item Not Found"),
+                actions: [
+                  fluent.Button(
+                    child: Text("OK"),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  )
+                ],
+              );
+            });
+      } else {
+        throw (Exception);
+      }
+    } catch (Exception) {
+      fluent.showDialog(
+          context: context,
+          builder: (context) {
+            return fluent.ContentDialog(
+              title: Text("Weather"),
+              content: Text("Check the Internet Connection \n ${Exception}"),
+              actions: [
+                fluent.Button(
+                  child: Text("OK"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            );
+          });
+    }
+    setState(() => {});
+  }
+
+  @override
+  void initState() {
+    callapi();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return fluent.NavigationView(
+      pane: fluent.NavigationPane(
+        selected: _pane_one,
+        displayMode: fluent.PaneDisplayMode.compact,
+        onChanged: (index) {
+          setState(() {
+            _pane_one = index;
+          });
+        },
+        footerItems: [
+          fluent.PaneItemSeparator(),
+          fluent.PaneItem(
+              icon: const Icon(fluent.FluentIcons.settings),
+              title: const Text('Change City'),
+              onTap: () => fluent.showDialog(
+                  context: context,
+                  builder: (context) {
+                    return fluent.ContentDialog(
+                      title: Text("Change City"),
+                      content: fluent.Container(
+                        height: 50,
+                        child: Column(
+                          children: [
+                            fluent.TextFormBox(
+                              controller: tc,
+                            )
+                          ],
+                        ),
+                      ),
+                      actions: [
+                        fluent.Button(
+                          child: Text("OK"),
+                          onPressed: () {
+                            setState(() {
+                              if (!tc.text.isEmpty) {
+                                q = tc.text;
+                              }
+                              callapi();
+                              Navigator.pop(context);
+                            });
+                          },
+                        )
+                      ],
+                    );
+                  }),
+              body: windows_ui(
+                  key: null,
+                  city_name: city_name,
+                  temp: temp,
+                  fl: fl,
+                  temp_max: temp_max,
+                  temp_min: temp_min,
+                  ico_url: ico_url,
+                  desc: desc,
+                  pressure: pressure,
+                  humidity: humidity,
+                  forecast_list: _forecast_list))
+        ],
+      ),
     );
   }
 }
@@ -148,6 +355,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     callapi();
+
     super.initState();
   }
 
@@ -161,27 +369,31 @@ class _MyHomePageState extends State<MyHomePage> {
               size: 20,
             ),
             onTap: () {
-              /* callapi();*/
-              //setState(() => Future(()=> callapi()));
+              callapi();
             },
           ),
           middle: GestureDetector(
-              child: Icon(
-            CupertinoIcons.cloud_fill,
-            size: 40,
-          ),onTap: (){
-                showCupertinoDialog(context: context, builder: (BuildContext context) => CupertinoAlertDialog(
-                  title: Text("About Weather"),
-                  content: Text("This is minor project \n Developed by Khushal Roop Rai"),
-                    actions: <Widget>[
-                CupertinoDialogAction(
-                child: Text("OK"),
-                  onPressed: () {
-
-                  Navigator.pop(context);
-                  },
-                )]));
-          },),
+            child: Icon(
+              CupertinoIcons.cloud_fill,
+              size: 40,
+            ),
+            onTap: () {
+              showCupertinoDialog(
+                  context: context,
+                  builder: (BuildContext context) => CupertinoAlertDialog(
+                          title: Text("About Weather"),
+                          content: Text(
+                              "This is minor project \n Developed by Khushal Roop Rai"),
+                          actions: <Widget>[
+                            CupertinoDialogAction(
+                              child: Text("OK"),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            )
+                          ]));
+            },
+          ),
           trailing: GestureDetector(
             child: Icon(
               CupertinoIcons.settings,
---
+++ weather/lib/windows_ui.dart
@@ -0,0 +1,310 @@
+import 'dart:ui';
+import 'package:flutter/animation.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart' as mat;
+import 'Forecast_Model.dart';
+import 'dart:math';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class windows_ui extends StatefulWidget {
+  String city_name = "cityname";
+  String temp = "temp";
+  String ico_url = "ico_url";
+  String desc = "desc";
+  String fl = 'fl';
+  String temp_max = 'temp+max';
+  String temp_min = 'temp_min';
+  String pressure = 'pressure';
+  String humidity = 'humidity';
+  List<Forecast_Model> forecast_list;
+
+  windows_ui(
+      {Key? key,
+        required this.city_name,
+        required this.temp,
+        required this.ico_url,
+        required this.desc,
+        required this.fl,
+        required this.temp_max,
+        required this.temp_min,
+        required this.pressure,
+        required this.humidity,
+        required this.forecast_list})
+      : super(key: key);
+
+  @override
+  State<windows_ui> createState() => _windows_uiState();
+
+}
+
+class _windows_uiState extends State<windows_ui> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+      ClipRect(
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(
+                sigmaX: 3,
+                sigmaY: 3,
+              ),
+              child: Image.network(
+                (widget.city_name == "Jodhpur")
+                    ? 'https://r4.wallpaperflare.com/wallpaper/683/259/650/india-india-jodhpur-blue-city-wallpaper-6f703d2eeabc1c8bc3939792efec6db3.jpg'
+                    : (widget.city_name == "Jaipur")
+                    ? 'https://images.unsplash.com/photo-1603262110263-fb0112e7cc33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'
+                    : (widget.city_name == "Mumbai")
+                    ? 'https://images.unsplash.com/photo-1529253355930-ddbe423a2ac7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bXVtYmFpfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+                    : (widget.city_name == "Jaisalmer")
+                    ? 'https://images.unsplash.com/photo-1589510337472-4307ab0087f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+                    : (widget.city_name == 'London')
+                    ? 'https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'
+                    : 'https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+                fit: BoxFit.fill,
+            )),
+          ),
+        SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                child: Stack(
+                  children: [
+                    Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(0, 25, 0, 0),
+                              child: Container(
+                                  width: 500,
+                                  height: 300,
+                                  child: mat.Card(
+                                    color: (widget.city_name == "Jodhpur")
+                                        ? Color(0x971A237E)
+                                        : (widget.city_name == 'Jaipur')
+                                        ? Color(0x71FF00AD)
+                                        : (widget.city_name == "Jaisalmer")
+                                        ? Color(0x71FFAD02)
+                                        : Color(0x35FFFFFF),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(15)),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        Row(
+                                            mainAxisSize: MainAxisSize.max,
+                                            mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                            children: [
+                                              Text(widget.city_name,
+                                                  style: GoogleFonts.poppins(
+                                                      fontSize: 35,
+                                                      color: Colors.white))
+                                            ]),
+                                        Row(
+                                          mainAxisSize: MainAxisSize.max,
+                                          mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                          children: [
+                                            Image.network(
+                                              widget.ico_url,
+                                              alignment: Alignment.center,
+                                            )
+                                          ],
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                          children: [
+                                            Text(widget.temp,
+                                                style: GoogleFonts.poppins(
+                                                    fontSize: 35,
+                                                    color: Colors.white))
+                                          ],
+                                        ),
+                                        Row(
+                                          mainAxisSize: MainAxisSize.max,
+                                          mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                          children: [
+                                            Text(widget.desc,
+                                                style: GoogleFonts.poppins(
+                                                    fontSize: 35,
+                                                    color: Colors.white))
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  )),
+                            ),
+                            Padding(padding: EdgeInsets.all(15)),
+                            Text(
+                              "Description",
+                              style: GoogleFonts.poppins(
+                                  fontSize: 30, color: Colors.white),
+                            ),
+                            Padding(padding: EdgeInsets.all(10)),
+                            Container(
+                              width: 500,
+                              height: 290,
+                              child: mat.Card(
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  color: (widget.city_name == "Jodhpur")
+                                      ? Color(0x971A237E)
+                                      : (widget.city_name == 'Jaipur')
+                                      ? Color(0x71FF00AD)
+                                      : (widget.city_name == "Jaisalmer")
+                                      ? Color(0x71FFAD02)
+                                      : Color(0x35FFFFFF),
+                                  elevation: 10,
+                                  child: Column(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        Column(
+                                          mainAxisSize: MainAxisSize.max,
+                                          children: [
+                                            GridView(
+                                                padding: EdgeInsets.all(5),
+                                                gridDelegate:
+                                                SliverGridDelegateWithFixedCrossAxisCount(
+                                                  crossAxisCount: 2,
+                                                  crossAxisSpacing: 5,
+                                                  mainAxisSpacing: 5,
+                                                  childAspectRatio: 5,
+                                                ),
+                                                shrinkWrap: true,
+                                                scrollDirection: Axis.vertical,
+                                                children: [
+                                                  Text(
+                                                    'Feels Like',
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 20,
+                                                        color: Colors.white),
+                                                  ),
+                                                  Text(
+                                                    widget.fl,
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 20,
+                                                        color: Colors.white),
+                                                    textAlign: TextAlign.end,
+                                                  ),
+                                                  Text(
+                                                    'Max Temperature',
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 20,
+                                                        color: Colors.white),
+                                                  ),
+                                                  Text(
+                                                    widget.temp_max,
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 20,
+                                                        color: Colors.white),
+                                                    textAlign: TextAlign.end,
+                                                  ),
+                                                  Text(
+                                                    'Min Temperature',
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 20,
+                                                        color: Colors.white),
+                                                  ),
+                                                  Text(
+                                                    widget.temp_min,
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 20,
+                                                        color: Colors.white),
+                                                    textAlign: TextAlign.end,
+                                                  ),
+                                                  Text(
+                                                    'Pressure',
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 20,
+                                                        color: Colors.white),
+                                                  ),
+                                                  Text(
+                                                    widget.pressure,
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 20,
+                                                        color: Colors.white),
+                                                    textAlign: TextAlign.end,
+                                                  ),
+                                                  Text(
+                                                    'Humidity',
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 20,
+                                                        color: Colors.white),
+                                                  ),
+                                                  Text(
+                                                    widget.humidity,
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 20,
+                                                        color: Colors.white),
+                                                    textAlign: TextAlign.end,
+                                                  ),
+                                                ])
+                                          ],
+                                        ),
+                                      ])),
+                            ),
+                            Padding(padding: EdgeInsets.all(8)),
+                            Text(
+                              "3 Hour Forecast",
+                              style: GoogleFonts.poppins(
+                                  fontSize: 30, color: Colors.white),
+                            ),
+                            Padding(padding: EdgeInsets.all(10)),
+                            Container(
+                                width: 500,
+                                height: 250,
+                                child: mat.Card(
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  color: (widget.city_name == "Jodhpur")
+                                      ? Color(0x971A237E)
+                                      : (widget.city_name == 'Jaipur')
+                                      ? Color(0x71FF00AD)
+                                      : (widget.city_name == "Jaisalmer")
+                                      ? Color(0x71FFAD02)
+                                      : Color(0x35FFFFFF),
+                                  elevation: 10,
+                                  child: ListView.builder(
+                                    itemBuilder: (BuildContext context, int index) {
+                                      String time = widget.forecast_list[index].time
+                                          .toString();
+                                      String icon = widget.forecast_list[index].icon
+                                          .toString();
+                                      String desc = widget
+                                          .forecast_list[index].description
+                                          .toString();
+                                      return ListTile(
+                                        leading: Text(
+                                          time,
+                                          style: GoogleFonts.poppins(
+                                              color: Colors.white),
+                                        ),
+                                        title: Text(desc,
+                                            style: GoogleFonts.poppins(
+                                                color: Colors.white,),textAlign: TextAlign.center),
+                                        trailing: Image.network(icon,width: 45),
+                                      );
+                                    },
+                                    shrinkWrap: true,
+                                    itemCount: widget.forecast_list.length,
+
+                                  ),
+                                ))
+                          ],
+                        )),
+                  ],
+                )))
+      ],
+    );
+  }
+}
